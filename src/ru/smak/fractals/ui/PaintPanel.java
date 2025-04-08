2e9c4ee8d822f3eee715b823fce15286eac90a1b
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-04-08 22:46:52 +0300

    Заготовка для лекции

--- src/ru/smak/fractals/ui/PaintPanel.java
+++ src/ru/smak/fractals/ui/PaintPanel.java
@@ -7,27 +7,5 @@ import java.util.function.Consumer;
 
 public class PaintPanel extends JPanel {
 
-    private final ArrayList<Consumer<Graphics>> paintActions = new ArrayList<>();
-
-    public void addPaintAction(Consumer<Graphics> action){
-        paintActions.add(action);
-    }
-
-    public void removePaintAction(Consumer<Graphics> action){
-        paintActions.remove(action);
-    }
-
-    public void removeAllPaintActions(){
-        paintActions.clear();
-    }
-
-    @Override
-    public void paint(Graphics g){
-        if (g == null) return;
-        super.paint(g);
-        for(var paintAction: paintActions) {
-            paintAction.accept(g);
-        }
-    }
 
 }
