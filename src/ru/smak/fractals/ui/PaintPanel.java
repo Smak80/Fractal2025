03af10f6a5c09ff16e55dbb1bacc8a0ad301fbfb
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-04-08 22:47:15 +0300

    Заготовка для лекции

--- src/ru/smak/fractals/ui/PaintPanel.java
+++ src/ru/smak/fractals/ui/PaintPanel.java
@@ -1,11 +1,7 @@
 package ru.smak.fractals.ui;
 
 import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.function.Consumer;
 
 public class PaintPanel extends JPanel {
-
 
 }
